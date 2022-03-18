c6406a8f7d6da90ca22a37e649ed944185e91a8a
Krzystof Staroń <kisiel3141@gmail.com>
2022-03-18 22:02:11 +0100

    nwm

---
+++ daily.js
@@ -0,0 +1,5 @@
+module.export = {
+  claim: (msg, args) => {
+    msg.reply("tak")
+  }
+}
--- main.js
+++ main.js
@@ -2,14 +2,23 @@ const fs = require("fs");
 const {Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const Discord = require('discord.js');
-const {token} = require("./config.json")
+const {token, prefix} = require("./config.json")
 
 client.on('ready', () => {
   console.log(`Bot starto`);
 });
 
 client.on('messageCreate', (msg) => {
- // main functionality
+  let args = msg.content.split(" ");
+  if (msg.author.bot || args[0] != prefix) return;
+
+  switch (args[1]) {
+    case "daily":
+      require("./daily").daily(msg, args)
+      break;
+    default:
+
+  }
 }
 
 client.login(token);
