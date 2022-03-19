d7e8f631965695468116f8bd8022b833d3084e1c
Krzystof Staroń <kisiel3141@gmail.com>
2022-03-19 14:48:42 +0100

    "don't forget to set token in config.json

--- setup.js
+++ setup.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 if (!fs.existsSync("config.json")) fs.writeFileSync('config.json', JSON.stringify(require("./objects.json").config));
 if (!fs.existsSync("data/users.json")) fs.writeFileSync('data/users.json', "{}");
 
-console.log("setup finished")
+console.log("don't forget to set token in config.json")
