fc1c19aaded216f65efde793785e081f605c130f
Krzystof Staroń <kisiel3141@gmail.com>
2022-03-19 09:22:03 +0100

    fix user function

--- functions.js
+++ functions.js
@@ -5,11 +5,15 @@ module.exports = {
     return JSON.parse(fs.readFileSync("data/users.json"));
   },
   "fixUser": function(msg) {
-    const data = JSON.parse(fs.readFileSync("data/users.json"));
-    if (!data.hasOwnProperty(msg.author.id)) {
-      data[msg.author.id] = JSON.parse(fs.readFileSync("objects.json")).user;
+    let data = JSON.parse(fs.readFileSync("data/users.json"));
+    let user = data[msg.author.id];
 
-      fs.writeFileSync('data/users.json', JSON.stringify(data));
+    for (var propety in user) {
+      if (!user.hasOwnProperty(propety)) {
+        user[propety] = JSON.parse(fs.readFileSync("objects.json"))[propety];
+      }
     }
+    data[msg.author.id] = user;
+    fs.writeFileSync('data/users.json', JSON.stringify(data));
   }
 }
